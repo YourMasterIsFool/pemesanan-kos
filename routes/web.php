ea18666db4f6015dcaca45c09b0046c76b0aec5d
yofan2408 <yofan.ixe@gmail.com>
2022-08-27 16:09:37 +0700

    fix auth

--- routes/web.php
+++ routes/web.php
@@ -59,9 +59,9 @@ Route::prefix('client')->middleware(['auth'])->group(function () {
 Route::prefix('account')->group(function () {
     Route::get('/profil', [AuthController::class, 'profileView'])->middleware(['auth'])->name('user.profil.view');
     Route::post('/profil', [AuthController::class, 'updateProfil'])->middleware(['auth'])->name('user.profil.update');
-    Route::get('/login', [AuthController::class, 'loginView'])->middleware('guest')->name('user.login.view');
+    Route::get('/login', [AuthController::class, 'loginView'])->name('user.login.view');
     Route::post('/login', [AuthController::class, 'login'])->name('user.login.auth');
-    Route::get('/register', [AuthController::class, 'registerView'])->middleware('guest')->name('user.register.view');
+    Route::get('/register', [AuthController::class, 'registerView'])->name('user.register.view');
     Route::post('/register', [AuthController::class, 'register'])->name('user.register.create');
     Route::post('/logout', [AuthController::class, 'logout'])->name('user.logout');
 });
